7746706209c400bc720f15034534b7b80a0872f4
Tanner Hornsby <thornsby@uchicago.edu>
2023-12-31 23:54:27 -0600

    added item type nav

---
+++ src/components/ui/item-type-list.tsx
@@ -0,0 +1,166 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import { Button } from '@/components/ui/button';
+import { Skeleton } from './skeleton';
+import {
+    ToggleGroup,
+    ToggleGroupItem,
+} from "@/components/ui/toggle-group"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { buttonVariants } from "@/components/ui/button"
+
+/*
+<Button variant='outline' className='border-2 border-foreground'>
+                                                More
+                                            </Button>
+*/
+
+export interface ItemType {
+    name: string;
+    link: string;
+    dbFilters: string[];
+    subTypes: string[];
+}
+
+const onSubTypeClick = (itemType: ItemType) => {
+    console.log(itemType.name)
+}
+
+interface Props {
+    itemTypes: ItemType[];
+    onClick?: (itemType: ItemType) => void;
+}
+
+export const HorizontalItemTypeList: React.FC<Props> = ({ itemTypes, onClick }) => {
+    // on mount, use the viewport to calculate the number of itemTypes that can be shown assuming
+    // their width is 160px
+    const [visibleItemCount, setVisibleItemCount] = useState(0);
+    // get the path of the current page and use it to determin
+    // active game
+    // active itemType
+    // active itemSubType
+
+    // while the state is zero, render the skeleton
+    // once the state is not zero, render the actual component
+
+    const handleResize = () => {
+        // get the viewport width
+        const viewportWidth = window.innerWidth;
+        // calculate the number of itemTypes that can fit in the viewport
+        const newVisibleItemCount = Math.floor(viewportWidth / 130);
+        // set the visibleItemCount state
+        setVisibleItemCount(newVisibleItemCount);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        // get the viewport width
+        const viewportWidth = window.innerWidth;
+        // calculate the number of itemTypes that can fit in the viewport
+        const newVisibleItemCount = Math.floor(viewportWidth / 130);
+        // set the visibleItemCount state
+        console.log('mounted visible count is', newVisibleItemCount)
+        setVisibleItemCount(newVisibleItemCount);
+
+        console.log('mounted visible count is', newVisibleItemCount)
+
+        // Cleanup the event listener on component unmount
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    if (visibleItemCount === 0) {
+        return (
+            <div className='flex space-x-4'>
+                {itemTypes.map((itemType) => (
+                    <Skeleton key={itemType.name} className='w-160 h-40' />
+                ))}
+            </div>
+        );
+    }
+
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <div className='flex justify-between w-full'>
+                    {itemTypes.slice(0, visibleItemCount).map((itemType) => (
+                        <NavigationMenuItem key={itemType.name}>
+                            <NavigationMenuTrigger>{itemType.name}</NavigationMenuTrigger>
+                            <NavigationMenuContent className='left-0 px-4 w-full'>
+                                <div className='p-4 grid grid-rows-2'>
+                                    <ToggleGroup type="multiple">
+                                        <div className='w-full flex justify-between'>
+                                            <div className='flex w-full justify-start space-x-4'>
+                                                <Button variant='outline' className='border-2 border-foreground'>
+                                                    All
+                                                </Button>
+                                                {itemType.subTypes && (
+                                                    <div className='flex'>
+                                                        <div className='flex'>
+                                                            {itemType.subTypes.slice(0, visibleItemCount).map((subType) => (
+                                                                <ToggleGroupItem value={subType} className='text-foreground hover:text-foreground' aria-label={`toggle ${subType}`} onClick={() => onSubTypeClick(itemType)}>
+                                                                    {subType}
+                                                                </ToggleGroupItem>
+                                                            ))}
+                                                        </div>
+                                                    </div>
+                                                )}
+                                            </div>
+                                            {itemType.subTypes.slice(visibleItemCount).length > 0 && (
+                                                <Popover>
+                                                    <PopoverTrigger><span className={buttonVariants({ variant: "ghost" })}>More</span></PopoverTrigger>
+                                                    <PopoverContent>
+                                                        {itemType.subTypes.slice(visibleItemCount).map((subType) => (
+                                                            <ToggleGroupItem value={subType} className='text-foreground hover:text-foreground' aria-label={`toggle ${subType}`} onClick={() => onSubTypeClick(itemType)}>
+                                                                {subType}
+                                                            </ToggleGroupItem>
+                                                        ))}
+                                                    </PopoverContent>
+                                                </Popover>
+                                            )}
+                                        </div>
+                                    </ToggleGroup>
+                                    Carousel
+                                </div>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    ))}
+                    {itemTypes.slice(visibleItemCount).length > 0 && (
+                        <NavigationMenuItem key={'more'}>
+                            <NavigationMenuTrigger>
+                                More
+                            </NavigationMenuTrigger>
+                            <NavigationMenuContent className='right-0 px-2'>
+                                <NavigationMenuList className='w-full flex flex-col'>
+                                    {itemTypes.slice(visibleItemCount).map((itemType) => (
+                                        <Link href={itemType.link} legacyBehavior passHref>
+                                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                                {itemType.name}
+                                            </NavigationMenuLink>
+                                        </Link>
+                                    ))}
+                                </NavigationMenuList>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    )}
+                </div>
+            </NavigationMenuList >
+        </NavigationMenu >
+    );
+};
+
--- src/components/ui/top-nav.tsx
+++ src/components/ui/top-nav.tsx
@@ -7,35 +7,10 @@ import { Input } from "@/components/ui/input";
 import UserSection from "@/components/ui/user-section";
 import { IconButton } from './icon-button';
 import * as React from "react"
-
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
-export function SelectGame() {
-  return (
-    <Select>
-      <SelectTrigger className="w-min hidden lg:flex border-none focus:ring-transparent focus-border-b-2">
-        <SelectValue placeholder="CS2" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Games</SelectLabel>
-          <SelectItem value="cs2">CS2</SelectItem>
-          <SelectItem value="dota">DOTA</SelectItem>
-          <SelectItem value="tf2">TF2</SelectItem>
-          <SelectItem value="rust">RUST</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  )
-}
+import { Separator } from '@/components/ui/separator';
+import { HorizontalItemTypeList } from '@/components/ui/item-type-list';
+import { ItemType } from './item-type-list';
+import { Skeleton } from './skeleton';
 
 // import search icon, bell icon, profile icon, and cart icon outlines and solids
 // from lucide react
@@ -46,29 +21,139 @@ export const fontSans = FontSans({
     variable: "--font-sans",
 })
 
+const dummyTypes: ItemType[] = [
+    {
+        name: "Pistols",
+        link: "csgo/items/pistols",
+        dbFilters: ["pistols"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "SMGs",
+        link: "csgo/items/smg",
+        dbFilters: ["smgs"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "Rifles",
+        link: "csgo/items/rifles",
+        dbFilters: ["rifles"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "Snipers",
+        link: "csgo/items/snipers",
+        dbFilters: ["snipers"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "Shotguns",
+        link: "csgo/items/shotguns",
+        dbFilters: ["shotguns"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "Machine Guns",
+        link: "csgo/items/machine-guns",
+        dbFilters: ["machine-guns"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "Gloves",
+        link: "csgo/items/gloves",
+        dbFilters: ["gloves"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "Knives",
+        link: "csgo/items/knives",
+        dbFilters: ["knives"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "Stickers",
+        link: "csgo/items/stickers",
+        dbFilters: ["stickers"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "Music Kits",
+        link: "csgo/items/music-kits",
+        dbFilters: ["music-kits"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "Graffiti",
+        link: "csgo/items/graffiti",
+        dbFilters: ["graffiti"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    },
+    {
+        name: "Agents",
+        link: "csgo/items/agents",
+        dbFilters: ["agents"],
+        subTypes: ['shadowed', 'horizon', 'gouda', 'beli', 'cheese', 'hats', 'goo bad']
+    }
+]
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+export function SelectGame() {
+    return (
+        <Select>
+            <SelectTrigger className="w-min hidden lg:flex border-none focus:ring-transparent focus-border-b-2">
+                <SelectValue placeholder="CS2" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectLabel>Games</SelectLabel>
+                    <SelectItem value="cs2">CS2</SelectItem>
+                    <SelectItem value="dota">DOTA</SelectItem>
+                    <SelectItem value="tf2">TF2</SelectItem>
+                    <SelectItem value="rust">RUST</SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    )
+}
+
 
 export default function TopNav() {
     return (
-        <nav className={cn("flex items-center justify-between p-2", fontSans.variable)}>
-            <div className='flex items-center justify-between'>
-                <IconButton icon={<Menu/>} className="flex lg:hidden"/>
-                <h1 className="text-2xl font-bold mx-2">Logo</h1>
-                <SelectGame />
-                <Button variant='ghost' className="hidden sm:flex items-center justify-center p-2 ">
-                    MARKET
-                </Button>
-                <div className="hidden sm:flex items-center justify-between space-x-2 md:ml-32 lg:ml-48">
-                    <Search className='w-8 h-8 p-2 -mr-3' />
-                    <Input
-                        className="hidden sm:block w-64 h-8 p-2 border-none focus-visible:ring-none"
-                        placeholder="Search"
-                    />
+        <div>
+            <nav className={cn("flex items-center justify-between p-2", fontSans.variable)}>
+                <div className='flex items-center justify-between'>
+                    <IconButton icon={<Menu />} className="flex lg:hidden" />
+                    <h1 className="text-2xl font-bold mx-2">Logo</h1>
+                    <SelectGame />
+                    <Button variant='ghost' className="hidden sm:flex items-center justify-center p-2 ">
+                        MARKET
+                    </Button>
+                    <div className="hidden sm:flex items-center justify-between space-x-2 md:ml-32 lg:ml-48">
+                        <Search className='w-8 h-8 p-2 -mr-3' />
+                        <Input
+                            className="hidden sm:block w-64 h-8 p-2 border-none focus-visible:ring-none"
+                            placeholder="Search"
+                        />
+                    </div>
+
                 </div>
-                
+                <div className="flex items-center justify-between space-x-4">
+                    <UserSection />
+                </div>
+            </nav>
+            <div className='transition ease-in-out hidden md:flex flex-col items-center justify-center'>
+                <Separator className='w-11/12' />
+                <HorizontalItemTypeList itemTypes={dummyTypes} />
             </div>
-            <div className="flex items-center justify-between space-x-4">
-                <UserSection />
-            </div>
-        </nav>
+        </div>
     );
 }
